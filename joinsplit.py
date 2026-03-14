35d0fd2010231b0a3e2d631dbd743573bd5dc637
sanjay <sanjaysugumar2005@gmail.com>
2026-03-15 01:27:47 +0530

    today class

--- joinsplit.py
+++ joinsplit.py
@@ -1,5 +1,4 @@
-###################split####################
-
+########################### SPLIT #################
 # menu="100,idly,30"
 # item=menu.split(",")
 # id=item[0]
@@ -7,15 +6,30 @@
 # price=int(item[2])+20
 # join=",".join([id,name,str(price)])
 # print(join)
-
-################## FIND##################
+########################### FIND ########################
+# message="learning is fun"
+# print(message[12:15])
 
 # message="learning is fun"
 # pos=message.find("fun")
 # print(message[pos:pos+3])
 
-# message=("learning is fun")
-# print(message[12:15])
+#################################
 
-########################################
+# template="hi {0} order for {1} placed"
+# out = template.format("sanjay","1000")
+# print(out)
+# out2 = template.format("arun","2000")
+# print(out2)
 
+#########################FORMAT##############
+
+# template = "hi {0} order for {1} placed"
+
+# out = template.format("sanjay","1000")
+# print(out)
+
+# out2 = template.format("arun","2000")
+# print(out2)
+
+############################################

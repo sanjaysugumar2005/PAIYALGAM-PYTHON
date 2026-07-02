a0bb534b6a2df0a915b1c7aa34f699af1cd52b96
sanjaysugumar2005 <sanjaysugumar2005@gmail.com>
2026-07-02 23:08:02 +0530

    five question

---
+++ bulit.py
@@ -0,0 +1,63 @@
+# Reverse a string without using built-in functions.
+
+# text=input("enter a name: ")
+# reverse=""
+# for i in range(len(text)-1,-1,-1):
+#     reverse+=text[i]
+# print("reverse string",reverse)
+
+# Check whether a number is prime.
+
+# num = int(input("Enter a number: "))
+
+# if num <= 1:
+#     print("Not Prime")
+# else:
+#     for i in range(2, num):
+#         if num % i == 0:
+#             print("Not Prime")
+#             break
+#     else:
+#         print("Prime")
+
+
+# Find the factorial of a number.
+
+# num = int(input("Enter a number: "))
+
+# factorial = 1
+
+# for i in range(1, num + 1):
+#     factorial *= i
+
+# print("Factorial =", factorial)
+
+# Generate the Fibonacci series up to n terms.
+
+# n = int(input("Enter number of terms: "))
+
+# a = 0
+# b = 1
+
+# for i in range(n):
+#     print(a, end=" ")
+#     c = a + b
+#     a = b
+#     b = c
+
+
+
+# Check whether a string is a palindrome.
+
+
+# text = input("Enter a string: ")
+
+# reverse = ""
+
+# for i in range(len(text) - 1, -1, -1):
+#     reverse += text[i]
+
+# if text == reverse:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")

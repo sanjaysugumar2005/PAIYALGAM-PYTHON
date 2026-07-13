53e6757eb5454376693a46bbbd4de0b3b331c617
sanjaysugumar2005 <sanjaysugumar2005@gmail.com>
2026-07-13 21:49:30 +0530

    WORK

--- practice.py
+++ practice.py
@@ -29,3 +29,32 @@
 #     print("Not Palindrome")
 
 
+######################## 
+
+# a = 10
+
+# if a == 10:
+#     print("hello")
+# else:
+#     print("hello")
+
+
+######################## 
+
+# numbers = [10, 20, 30, 40, 35, 15, 5]
+
+# print(max(numbers))
+
+
+######################## 
+
+
+# numbers = [10, 20, 30, 40, 35, 15, 5]
+
+# maximum = numbers[0]
+
+# for num in numbers:
+#     if num > maximum:
+#         maximum = num
+
+# print(maximum)

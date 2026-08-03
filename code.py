84fbe3acc91f90b9fea80bb770428a8ca294bc55
sanjaysugumar2005 <sanjaysugumar2005@gmail.com>
2026-08-03 22:42:05 +0530

    code

---
+++ code.py
@@ -0,0 +1,27 @@
+# def reverse_string(s):
+#     return s[::-1]
+
+# text = input("Enter a string: ")
+# print("Reversed:", reverse_string(text))
+
+def add(no1, no2 ):
+    return no1+no2
+def subration(no1,no2):
+    return no1-no2
+def divide(no1,no2):
+    return no1//no2
+developer="nirmal anad"
+import function_demo
+result=function_demo.add(100,150)
+print(result)
+
+# def add(no1, no2):
+#     return no1 + no2
+
+# def subtraction(no1, no2):
+#     return no1 - no2
+
+# def divide(no1, no2):
+#     return no1 // no2
+
+# developer = "Nirmal Anand"
